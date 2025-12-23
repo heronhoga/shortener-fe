b5bc5d3e33af56befea687d90f2e284ece823bf9
heronhoga <nozzeravixer@gmail.com>
2025-12-23 10:40:15 +0700

    feat:add home page

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-50 overflow-hidden">
+    <div class="text-center max-w-md px-6">
+      <h1 class="text-4xl font-extrabold text-gray-900">
+        Snipe
+      </h1>
+
+      <p class="mt-4 text-gray-600">
+        Make the <span class="italic">asfasfngso1akbnzjsdbesolrgaegaeigqaasasfsadvadvaefaeadvdaaskjbjdosbsepgeqirobeujogbshdbsjagjsbojsoib.com/kwaegbwuogwjbfo13?name=laasdwasgjhbgo305310bejkkdskjeszjsdbnkzsbdxdjgepgibqepjbosdlkgnsdlgbsoegsjlbgkxcmbxbsjldbgsjeghowesjglbsjogobswsdogjsogbobo</span>
+      </p>
+      <p class="text-gray-600">to be <strong class="text-gray-900">SHORTER</strong></p>
+
+      <div class="mt-8 flex justify-center gap-4">
+        <router-link
+          to="/login"
+          class="px-6 py-3 rounded-lg bg-slate-600 text-white font-medium hover:bg-slate-700 transition"
+        >
+          Login
+        </router-link>
+
+        <router-link
+          to="/register"
+          class="px-6 py-3 rounded-lg border border-gray-300 text-gray-700 font-medium hover:bg-gray-100 transition"
+        >
+          Register
+        </router-link>
+      </div>
+    </div>
+    <div class="py-6">
+        <p class="text-gray-400">by hoga btw.</p>
+    </div>
+  </div>
+</template>
